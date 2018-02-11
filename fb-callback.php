f0c17c253365c0f88ab60b2a060c3cbb0a418019
Puwadon Sricharoen <jimmy18dev@gmail.com>
2018-02-11 17:55:13 +0700

    Changed.

--- fb-callback.php
+++ fb-callback.php
@@ -1,36 +1,80 @@
 <?php
 session_start();
-echo'Facebook Callback...<br>';
+echo'<h1>Facebook Callback...</h1>';
 
 require_once __DIR__ . '/facebook-sdk-v5/autoload.php'; // change path as needed
 
 $fb = new \Facebook\Facebook([
-  'app_id' => $_SESSION['AppID'],
-  'app_secret' => $_SESSION['AppSecret'],
-  'default_graph_version' => 'v2.10',
+  'app_id'                => $_SESSION['AppID'],
+  'app_secret'            => $_SESSION['AppSecret'],
+  'default_graph_version' => 'v2.12',
   //'default_access_token' => '{access-token}', // optional
 ]);
 
-// Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
-//   $helper = $fb->getRedirectLoginHelper();
-//   $helper = $fb->getJavaScriptHelper();
-//   $helper = $fb->getCanvasHelper();
-//   $helper = $fb->getPageTabHelper();
+$helper = $fb->getRedirectLoginHelper();
 
 try {
-  // Get the \Facebook\GraphNodes\GraphUser object for the current user.
-  // If you provided a 'default_access_token', the '{access-token}' is optional.
-  $response = $fb->get('/me', '{access-token}');
-} catch(\Facebook\Exceptions\FacebookResponseException $e) {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
   // When Graph returns an error
   echo 'Graph returned an error: ' . $e->getMessage();
   exit;
-} catch(\Facebook\Exceptions\FacebookSDKException $e) {
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
   // When validation fails or other local issues
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
   exit;
 }
 
-$me = $response->getGraphUser();
-echo 'Logged in as ' . $me->getName();
+if (! isset($accessToken)) {
+  if ($helper->getError()) {
+    header('HTTP/1.0 401 Unauthorized');
+    echo "Error: " . $helper->getError() . "\n";
+    echo "Error Code: " . $helper->getErrorCode() . "\n";
+    echo "Error Reason: " . $helper->getErrorReason() . "\n";
+    echo "Error Description: " . $helper->getErrorDescription() . "\n";
+  } else {
+    header('HTTP/1.0 400 Bad Request');
+    echo 'Bad request';
+  }
+  exit;
+}
+
+// Logged in
+echo '<h3>Access Token</h3>';
+var_dump($accessToken->getValue());
+
+// The OAuth 2.0 client handler helps us manage access tokens
+$oAuth2Client = $fb->getOAuth2Client();
+
+// Get the access token metadata from /debug_token
+$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+echo '<h3>Metadata</h3>';
+echo '<pre>';
+print_r($tokenMetadata);
+echo '</pre>';
+
+// Validation (these will throw FacebookSDKException's when they fail)
+$tokenMetadata->validateAppId($_SESSION['AppID']); // Replace {app-id} with your app id
+// If you know the user ID this access token belongs to, you can validate it here
+//$tokenMetadata->validateUserId('123');
+$tokenMetadata->validateExpiration();
+
+if (! $accessToken->isLongLived()) {
+  // Exchanges a short-lived access token for a long-lived one
+  try {
+    $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+  } catch (Facebook\Exceptions\FacebookSDKException $e) {
+    echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+    exit;
+  }
+
+  echo '<h3>Long-lived</h3>';
+  var_dump($accessToken->getValue());
+}
+
+$_SESSION['fb_access_token'] = (string) $accessToken;
+
+// User is logged in with a long-lived access token.
+// You can redirect them to a members-only page.
+//header('Location: https://example.com/members.php');
 ?>
